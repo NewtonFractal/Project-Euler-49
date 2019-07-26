b51b223406f83353d2bbbdab6711d92e9b245f74
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-26 18:35:22 +0200

    Update Project euler #49.py

--- Project euler #49.py
+++ Project euler #49.py
@@ -8,7 +8,7 @@ def permutationchecker(prime1,prime2,prime3):
     if len(permutationlist) <= 4:
         if int(prime1) != 1487:
             end = time.time()
-            print(prime1), print(prime2), print(prime3)
+            print(prime1+prime2+prime3)
             print(end - start)
             exit()
 
